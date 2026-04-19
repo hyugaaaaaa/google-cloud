babe5235acff361c88e36fca33c8e0b7f35fdcc6
hyugaaaaaa <hyuga0510@icloud.com>
2026-04-19 15:24:46 +0900

    fix: resolve skeleton type error and explanation syntax error

--- src/components/common/Skeleton.tsx
+++ src/components/common/Skeleton.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 interface SkeletonProps {
   className?: string
+  style?: React.CSSProperties
 }
 
-export function Skeleton({ className }: SkeletonProps) {
+export function Skeleton({ className, style }: SkeletonProps) {
   return (
     <div 
       className={`animate-pulse bg-qz-border dark:bg-[#2E3856] rounded-xl ${className}`} 
+      style={style}
     />
   )
 }
